667da1a1b583a07b99deb722bdd1a8b780703274
Gokberk Yaltirakli <webdosusb@gmail.com>
2015-06-16 21:16:26 +0300

    CDATA for escaping. Why xml, why?

--- get_servers.c
+++ get_servers.c
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
                 printf("%lu;%s|%lu;%s|%d|%d|%d\n", strlen(server->name), server->name, strlen(server->mapName), server->mapName, server->players, server->maxPlayers, server->bots);
             }else if(argc > 1 && strcmp(argv[1], "xml") == 0){
                 printf("    <server>\n");
-                printf("        <name>%s</name>\n", server->name);
-                printf("        <mapName>%s</mapName>\n", server->mapName);
+                printf("        <name><![CDATA[%s]]></name>\n", server->name);
+                printf("        <mapName><![CDATA[%s]]></mapName>\n", server->mapName);
                 printf("        <playerCount>%d</playerCount>\n", server->players);
                 printf("        <maxPlayers>%d</maxPlayers>\n", server->maxPlayers);
                 printf("        <botCount>%d</botCount>\n", server->bots);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
                 char **players; 
                 int playerCount = cs2d_get_players(server->address, &players);
                 for (int i=0;i<playerCount;i++){
-                    printf("            <player>%s</player>\n", players[i]);
+                    printf("            <player><![CDATA[%s]]></player>\n", players[i]);
                 }
                 printf("        </players>\n");
                 printf("    </server>\n");
